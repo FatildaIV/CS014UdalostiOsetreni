bccfee870e016d1bcd3316242192ffd773e14f3a
Fatilda IV <30930670+FatildaIV@users.noreply.github.com>
2021-06-11 11:22:49 +0200

    Add files via upload

---
+++ CS014UdalostiOsetreni/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace CS014UdalostiOsetreni
+{
+    partial class UdalostiOsetreniForm
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.souradniceLabel = new System.Windows.Forms.Label();
+            this.udalostiListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // souradniceLabel
+            // 
+            this.souradniceLabel.AutoSize = true;
+            this.souradniceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.souradniceLabel.Location = new System.Drawing.Point(638, 412);
+            this.souradniceLabel.Name = "souradniceLabel";
+            this.souradniceLabel.Size = new System.Drawing.Size(106, 29);
+            this.souradniceLabel.TabIndex = 0;
+            this.souradniceLabel.Text = "X: ?, Y: ?";
+            // 
+            // udalostiListBox
+            // 
+            this.udalostiListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.udalostiListBox.FormattingEnabled = true;
+            this.udalostiListBox.ItemHeight = 20;
+            this.udalostiListBox.Location = new System.Drawing.Point(454, 12);
+            this.udalostiListBox.Name = "udalostiListBox";
+            this.udalostiListBox.Size = new System.Drawing.Size(334, 384);
+            this.udalostiListBox.TabIndex = 1;
+            // 
+            // UdalostiOsetreniForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.udalostiListBox);
+            this.Controls.Add(this.souradniceLabel);
+            this.Name = "UdalostiOsetreniForm";
+            this.Text = "CS014 Ošetření událostí – Jakub Sirový";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.UdalostiOsetreniForm_MouseClick);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.UdalostiOsetreniForm_MouseDown);
+            this.MouseEnter += new System.EventHandler(this.UdalostiOsetreniForm_MouseEnter);
+            this.MouseLeave += new System.EventHandler(this.UdalostiOsetreniForm_MouseLeave);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.UdalostiOsetreniForm_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label souradniceLabel;
+        private System.Windows.Forms.ListBox udalostiListBox;
+    }
+}
+
---
+++ CS014UdalostiOsetreni/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS014UdalostiOsetreni
+{
+    public partial class UdalostiOsetreniForm : Form
+    {
+        private int pocetUdalosti = 0;
+
+        public UdalostiOsetreniForm()
+        {
+            InitializeComponent();
+        }
+
+        private void UdalostZaznam(string zprava) 
+        {
+            udalostiListBox.Items.Insert(0, String.Format("{0:D4} {1}", pocetUdalosti++, zprava));  // pocet++->pocet = pocet + 1
+        }
+
+        private string KurzorSouradnice(int x, int y)
+        {
+            return String.Format("X: {0:D3}, Y: {1:D3}", x, y);
+        }
+
+        private void UdalostiOsetreniForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            souradniceLabel.Text = KurzorSouradnice(e.X, e.Y);
+            UdalostZaznam(string.Format("MouseMove {0}", KurzorSouradnice(e.X, e.Y)));
+        }
+
+        private void UdalostiOsetreniForm_MouseClick(object sender, MouseEventArgs e)
+        {
+            UdalostZaznam("MouseClick");
+        }
+
+        private void UdalostiOsetreniForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            UdalostZaznam("MouseDown");
+        }
+
+        private void UdalostiOsetreniForm_MouseEnter(object sender, EventArgs e)
+        {
+            UdalostZaznam("MouseEnter");
+        }
+
+        private void UdalostiOsetreniForm_MouseLeave(object sender, EventArgs e)
+        {
+            UdalostZaznam("MouseLeave");
+        }
+    }
+}
